61db97f0e4131ad9449a09d6e6f47572d9054abe
LMB.osto <lokmaneboutiba@gmail.com>
2023-06-09 15:48:20 +0100

    creating new project and adding routs

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import Title from "./components/Title";
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import DashBoard from "./views/DashBoard";
+import Users from "./views/Users";
+import Details from "./views/Details";
+import Products from "./views/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          {/*<Title title="Dynamic"/>*/}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users/" element={<Users />} />
+          <Route path="/users/:userId" element={<Details />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+        <Title title="Static Foot" />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
